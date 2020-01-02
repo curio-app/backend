5603317ee93c5b1786d34884e445fc90be3bb82b
Kevin Smith <keveightysev@gmail.com>
2020-01-02 12:06:50 -0600

    Login route setup

--- auth/route.js
+++ auth/route.js
@@ -36,4 +36,29 @@ router.post('/register', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'Email address required' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'Password required' });
+  }
+  try {
+    const user = await Users.getUserByEmail(email);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ message: 'User signed in', token });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: 'Server error while logging in', error });
+  }
+});
+
 module.exports = router;

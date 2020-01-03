3c27d8cc50712cb22388a3563ef17277216fc933
Kevin Smith <keveightysev@gmail.com>
2020-01-02 19:07:19 -0600

    Added controller for getting all collectibles

--- collectibles/controller.js
+++ collectibles/controller.js
@@ -1,7 +1,44 @@
 const db = require('../config/db.js');
 
-const getAllCollectibles = () => {
-  return db('collectibles');
+const { getTagsByCollectibleId } = require('../tags/controller.js');
+const { getLikesByCollectibleId } = require('../likes/controller.js');
+
+const getAllCollectibles = async () => {
+  const collectibles = await db('folders')
+    .where({ 'folders.name': 'All' })
+    .join('users', 'folders.userId', 'users.id')
+    .join('foldersCollectibles', 'folders.id', 'foldersCollectibles.folderId')
+    .join(
+      'collectibles',
+      'foldersCollectibles.collectibleId',
+      'collectibles.id'
+    )
+    .select(
+      'collectibles.id as id',
+      'collectibles.name',
+      'collectibles.createdAt',
+      'collectibles.updatedAt',
+      'collectibles.imageUrl',
+      'collectibles.story',
+      'collectibles.description',
+      'collectibles.sellable',
+      'folders.id as folderId',
+      'users.email',
+      'users.id as userId'
+    )
+    .orderBy('collectibles.createdAt')
+    .limit(20);
+  const full = collectibles.map(async coll => {
+    const likes = await getLikesByCollectibleId(coll.id);
+    const tags = await getTagsByCollectibleId(coll.id);
+    return {
+      ...coll,
+      likes,
+      tags,
+    };
+  });
+  const results = await Promise.all(full);
+  return results;
 };
 
 const getCollectibleById = id => {
--- collectibles/route.js
+++ collectibles/route.js
@@ -2,12 +2,24 @@ const router = require('express').Router();
 
 const Collectibles = require('./controller.js');
 
+router.get('/', async (req, res) => {
+  try {
+    const collectibles = await Collectibles.getAllCollectibles();
+    res.status(200).json(collectibles);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: 'Server error fetching collectibles', error });
+  }
+});
+
 router.post('/', async (req, res) => {
   const collectible = req.body;
   console.log(collectible);
 
   Collectibles.addCollectible(collectible)
-    .then(collectible => res.status(201).json(collectible))
+    .then(newCollectible => res.status(201).json(newCollectible))
     .catch(error => {
       console.log(error);
       res
